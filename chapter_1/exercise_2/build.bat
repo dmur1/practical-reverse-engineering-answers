11562aea2a02d60f07479acc104d423e7fd405e1
dan <dan@oticsoftware.com>
2022-04-08 17:07:12 +0100

    exe to load the decomplication dll for testing

--- chapter_1/exercise_2/build.bat
+++ chapter_1/exercise_2/build.bat
@@ -9,7 +9,8 @@ if not exist build mkdir build
 pushd build
 del * /Q
 
-cl.exe /W3 /WX /c /O2 /Oy- /D_UNICODE /GS- ..\decompiled.cpp
+cl.exe /W3 /WX /O2 /Oy- /D_UNICODE /GS- /LD /Zi ..\decompiled.cpp
 dumpbin.exe /disasm decompiled.obj > code.asm
+cl.exe /W3 /WX /Od /Oy- /D_UNICODE /GS- /Zi ..\main.cpp
 
 popd
---
+++ chapter_1/exercise_2/main.cpp
@@ -0,0 +1,8 @@
+#include <Windows.h>
+#include <stdio.h>
+
+int main() {
+    LoadLibraryA( "decompiled.dll" );
+    getchar();
+    return 0;
+}

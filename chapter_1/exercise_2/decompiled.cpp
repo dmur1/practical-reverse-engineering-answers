06795826f623f9f3e0d04375f8e92c4f5ee3a036
dan <dan@oticsoftware.com>
2022-04-08 17:06:48 +0100

    more accurate decomplication ( still not perfect match )

--- chapter_1/exercise_2/decompiled.cpp
+++ chapter_1/exercise_2/decompiled.cpp
@@ -2,6 +2,7 @@
 #include <intrin.h>
 #include <tlhelp32.h>
 
+#pragma pack( 1 )
 struct IDTR {
     SHORT limit;
     DWORD base;
@@ -31,16 +32,29 @@ DllMain( HINSTANCE hinstDLL, DWORD fwdReason, LPVOID lpvReserved ) {
 
     process_entry.dwSize = 296;
 
+    DWORD process_id = fwdReason;
+    DWORD process_parent_id = fwdReason;
+
     if ( Process32First( snapshot, &process_entry ) ) {
         do {
             if ( _stricmp( process_name, process_entry.szExeFile ) == 0 )
             {
-                if ( fwdReason != DLL_PROCESS_ATTACH )
-                    CreateThread( 0, 0, ( LPTHREAD_START_ROUTINE )0x100032D0, 0, 0, 0 );
-                return TRUE;
+                process_parent_id = process_entry.th32ParentProcessID;
+                process_id = process_entry.th32ProcessID;
+                break;
             }
         } while ( Process32Next( snapshot, &process_entry ) );
     }
 
-    return FALSE;
+    if ( process_parent_id == process_id )
+    {
+        if ( fwdReason == DLL_PROCESS_ATTACH )
+        {
+            CreateThread( 0, 0, 0, (LPTHREAD_START_ROUTINE)0x100032D0, 0, 0 );
+        }
+
+        return true;
+    }
+
+    return 0;
 }

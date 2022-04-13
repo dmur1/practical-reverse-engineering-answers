52347b482754d421f02ec68ad88f02955f494403
dan <dan@oticsoftware.com>
2022-04-13 21:09:11 +0100

    better adherance to static branch prediction

--- chapter_1/exercise_4/strchr.asm
+++ chapter_1/exercise_4/strchr.asm
@@ -13,21 +13,21 @@ _strchr_1 proc
     repne scasb                                 ; search for '\0'
     not         ecx                             ; strlen( str ) + 1
     test        ebx, ebx                        ;
-    jnz         _strchr_1_loc_1                 ; if ( ch != '\0' )
-    lea         eax, [edi+ecx-1]                ;
-    jmp         _strchr_1_loc_2                 ; return &str[ strlen( str ) + 1 ]
-_strchr_1_loc_1:
+    jz          _strchr_1_loc_2                 ; if ( ch != '\0' )
     sub         edi, ecx                        ; char const* str
     mov         eax, ebx                        ; int ch
     repne scasb                                 ; search for ch
-    xor         eax, eax                        ;
+    xor         eax, eax
+    sub         edi, 1                          ; &str[ found ]
     test        ecx, ecx                        ;
-    jz          _strchr_1_loc_2                 ; if ( !found ) return 0
-    lea         eax, [edi-1]                    ; return &str[ found ]
-_strchr_1_loc_2:
+    cmovnz      eax, edi                        ; return found ? &str[ found ] : 0
+_strchr_1_loc_1:
     pop         ebx
     pop         edi
     ret
+_strchr_1_loc_2:
+    lea         eax, [edi+ecx-1]                ;
+    jmp         _strchr_1_loc_1                 ; return &str[ strlen( str ) + 1 ]
 _strchr_1 endp
 
 end

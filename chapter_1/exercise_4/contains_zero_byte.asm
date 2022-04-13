2b8b72ccd84f6fe045ffa42a5be35234737de67a
dan <dan@oticsoftware.com>
2022-04-13 19:57:11 +0100

    improve the zero byte test

--- chapter_1/exercise_4/contains_zero_byte.asm
+++ chapter_1/exercise_4/contains_zero_byte.asm
@@ -2,15 +2,11 @@
 .code
 
 _contains_zero_byte_ proc
-    push    ebx
-    xor     eax, eax
-    mov     ebx, [esp+8]
-    lea     ecx, [ebx-01010101h]
-    not     ebx
-    and     ecx, ebx
-    and     ecx, 80808080h
-    setnz   al
-    pop     ebx
+    mov     eax, [esp+4]
+    lea     ecx, [eax-01010101h]
+    not     eax
+    and     eax, 80808080h
+    and     eax, ecx
     ret
 _contains_zero_byte_ endp
 
--- chapter_1/exercise_4/contains_zero_byte_test.cpp
+++ chapter_1/exercise_4/contains_zero_byte_test.cpp
@@ -4,10 +4,16 @@
 extern "C" int contains_zero_byte_( int x );
 
 int contains_zero_byte( int x ) {
-    return ( ( ( x - 0x01010101 ) & ~x ) & 0x80808080 ) ? 1 : 0;
+    return ( x - 0x01010101 ) & ~x & 0x80808080;
 }
 
 int main() {
+    printf( "%u\n", contains_zero_byte_( 0 ) );
+    printf( "%u\n", contains_zero_byte_( 1 ) );
+    printf( "%u\n", contains_zero_byte_( 0x0100 ) );
+    printf( "%u\n", contains_zero_byte_( 0x010000 ) );
+    printf( "%u\n", contains_zero_byte_( 0x01000000 ) );
+    printf( "%u\n", contains_zero_byte_( 0x01010100 ) );
     printf( "%u\n", contains_zero_byte_( 0x01010101 ) );
     printf( "%u\n", contains_zero_byte_( 0x00010101 ) );
     printf( "%u\n", contains_zero_byte_( 0x01000101 ) );
@@ -16,11 +22,29 @@ int main() {
 
     printf( "\n" );
 
+    printf( "%u\n", contains_zero_byte( 0 ) );
+    printf( "%u\n", contains_zero_byte( 1 ) );
+    printf( "%u\n", contains_zero_byte( 0x0100 ) );
+    printf( "%u\n", contains_zero_byte( 0x010000 ) );
+    printf( "%u\n", contains_zero_byte( 0x01000000 ) );
+    printf( "%u\n", contains_zero_byte( 0x01010100 ) );
     printf( "%u\n", contains_zero_byte( 0x01010101 ) );
     printf( "%u\n", contains_zero_byte( 0x00010101 ) );
     printf( "%u\n", contains_zero_byte( 0x01000101 ) );
     printf( "%u\n", contains_zero_byte( 0x01010001 ) );
     printf( "%u\n", contains_zero_byte( 0x01010100 ) );
+
+    for ( uint32_t i = 0; i < 0xFFFFFFFF; ++i ) {
+        if ( ( ( ( i >> 24 ) & 0xFF ) == 0 ) || ( ( ( i >> 16 ) & 0xFF ) == 0 )
+            || ( ( ( i >> 8 ) & 0xFF ) == 0 ) || ( ( i & 0xFF ) == 0 ) ) {
+            if ( !contains_zero_byte_( i ) )
+                __debugbreak();
+        }
+        else {
+            if ( contains_zero_byte_( i ) )
+                __debugbreak();
+        }
+    }
 
     return 0;
 }

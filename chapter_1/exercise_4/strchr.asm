3b69b91cf6f29a82ed96f313be5271a3a13bb2ba
dan <dan@oticsoftware.com>
2022-04-14 19:59:06 +0100

    more work on strchr

--- chapter_1/exercise_4/strchr.asm
+++ chapter_1/exercise_4/strchr.asm
@@ -13,7 +13,7 @@ _strchr_1 proc
     repne scasb                                 ; search for '\0'
     not         ecx                             ; strlen( str ) + 1
     test        ebx, ebx                        ;
-    jz          _strchr_1_loc_2                 ; if ( ch != '\0' )
+    jz          _strchr_1_loc_1                 ; if ( ch != '\0' )
     sub         edi, ecx                        ; char const* str
     mov         eax, ebx                        ; int ch
     repne scasb                                 ; search for ch
@@ -21,13 +21,40 @@ _strchr_1 proc
     sub         edi, 1                          ; &str[ found ]
     test        ecx, ecx                        ;
     cmovnz      eax, edi                        ; return found ? &str[ found ] : 0
-_strchr_1_loc_1:
     pop         ebx
     pop         edi
     ret
-_strchr_1_loc_2:
-    lea         eax, [edi+ecx-1]                ; &str[ strlen( str ) ]
-    jmp         _strchr_1_loc_1                 ; return ch == \'0' ? &str[ strlen( str ) ] : ...
+_strchr_1_loc_1:
+    lea         eax, [edi+ecx-1]                ; return ch == \'0' ? &str[ strlen( str ) ] : ...
+    pop         ebx
+    pop         edi
+    ret
 _strchr_1 endp
+
+; char* strchr_2( char const* str, int ch )
+; using cmp
+_strchr_2 proc
+    push        edi
+    xor         ecx, ecx
+    mov         eax, [esp+8]                    ; char const* str
+    mov         edx, [esp+12]                   ; int ch
+_strchr_2_loc_1:
+    cmp         byte ptr [eax+ecx], 0           ; while ( *s++ != '\0' )
+    lea         ecx, [ecx+1]
+    jne         _strchr_2_loc_1
+_strchr_2_loc_2:
+    movsx       edi, byte ptr [eax]             ; for ( int i = 0; i < strlen( str ) + 1; ++i )
+    cmp         edi, edx                        ; if ( str[ i ] == ch )
+    je          _strchr_2_loc_3
+    inc         eax
+    dec         ecx
+    jne         _strchr_2_loc_2
+    xor         eax, eax
+    pop         edi
+    ret                                         ; return 0
+_strchr_2_loc_3:
+    pop         edi
+    ret                                         ; return &str[ found ]
+_strchr_2 endp
 
 end
--- chapter_1/exercise_4/test_strchr.cpp
+++ chapter_1/exercise_4/test_strchr.cpp
@@ -1,6 +1,7 @@
 #include <stdio.h>
 
 extern "C" char* strchr_1( char const* str, int ch );
+extern "C" char* strchr_2( char const* str, int ch );
 
 int main( int argc, char** argv ) {
     printf( "%s\n", strchr_1( "Test", 'z' ) );
@@ -8,7 +9,18 @@ int main( int argc, char** argv ) {
     printf( "%s\n", strchr_1( "Test", 's' ) );
     printf( "%s\n", strchr_1( "Test", 't' ) );
     printf( "%s\n", strchr_1( "This is a slightly longer test!", 'y' ) );
+    printf( "%s\n", strchr_1( "This is a slightly longer test!", ' ' ) );
     printf( "%s\n", strchr_1( "", 'y' ) );
     printf( "%s\n", strchr_1( "", '\0' ) );
+
+    printf( "%s\n", strchr_2( "Test", 'z' ) );
+    printf( "%s\n", strchr_2( "Test", 'e' ) );
+    printf( "%s\n", strchr_2( "Test", 's' ) );
+    printf( "%s\n", strchr_2( "Test", 't' ) );
+    printf( "%s\n", strchr_2( "This is a slightly longer test!", 'y' ) );
+    printf( "%s\n", strchr_2( "This is a slightly longer test!", ' ' ) );
+    printf( "%s\n", strchr_2( "", 'y' ) );
+    printf( "%s\n", strchr_2( "", '\0' ) );
+
     return 0;
 }
